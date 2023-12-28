7c37d061689facefe390d31a321342754d18bc38
Ivan Tukmakov <62853281+Vancastr@users.noreply.github.com>
2023-12-28 18:13:35 +0300

    Got TaskAdding

--- server.py
+++ server.py
@@ -6,11 +6,67 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook_out():
     if request.method == 'POST':
+        address = "https://api.airtable.com/v0/app6lyEHjT1Da9eqD/Tasks/"
+        api_key = "patX0YZPRODK1eZQT.6bb66da4cf03361055f032e8e64a6a01001888d2e25b5b775a37204885ae99a1"
+        fin = "Bearer " + api_key
+        headers = {
+            "Authorization": fin,
+            "Content-Type": "application/json"
+        }
         res = request.form.to_dict()
-        ID = res['data[FIELDS_BEFORE][ID]']
+        if res['event'] == 'ONTASKADD':
+            ID = res['data[FIELDS_AFTER][ID]']
+            print(res)
+            data = requests.get('https://viantec.bitrix24.ru/rest/345/7z6g7j7n1loz8nk5/task.item.list.json').json()
+            info = {
+                "records": [
+                    {
+                        "fields": {
+                            "Name": "Задача 4",
+                            "Status": "In progress",
+                            "Start date": "2023-12-13",
+                            "Deadline": "2023-12-30"
+                        }
+                    }
+                ]
+            }
+            for i in data['result']:
+                if i['ID'] == ID:
+                    info['records'][0]['fields']['Name'] = i['TITLE']
+                    info['records'][0]['fields']['Deadline'] = i['DEADLINE'][:10]
+                    info['records'][0]['fields']['Start date'] = i['CREATED_DATE'][:10]
+                    if i['REAL_STATUS'] == '1':
+                        info['records'][0]['fields']['Status'] = 'New'
+                    elif i['REAL_STATUS'] == '2':
+                        info['records'][0]['fields']['Status'] = 'To do'
+                    elif i['REAL_STATUS'] == '3':
+                        info['records'][0]['fields']['Status'] = 'In progress'
+                    elif i['REAL_STATUS'] == '4':
+                        info['records'][0]['fields']['Status'] = 'Waiting for approval'
+                    elif i['REAL_STATUS'] == '5':
+                        info['records'][0]['fields']['Status'] = 'Done'
+            print(info)
+            response = requests.post(address, headers=headers, json=data)
+            print(response.text)
+        elif res['event'] == 'ONTASKUPDATE':
+            ID = res['data[FIELDS_AFTER][ID]']
+        elif res['event'] == 'ONTASKDELETE':
+            ID = res['data[FIELDS_BEFORE][ID]']
         print(res)
         data = requests.get('https://viantec.bitrix24.ru/rest/345/7z6g7j7n1loz8nk5/task.item.list.json').json()
-        info = {}
+        info = {
+                "records": [
+                    {
+                        "fields": {
+                            "Name": "Задача 4",
+                            "Status": "In progress",
+                            "Start date": "2023-12-13",
+                            "Deadline": "2023-12-30"
+                        }
+                    }
+                ]
+            }
+        
         for i in data['result']:
             if i['ID'] == ID:
                 info['TITLE'] = i['TITLE']
@@ -18,6 +74,7 @@ def webhook_out():
                 info['CREATED_DATE'] = i['CREATED_DATE']
                 info['STATUS'] = i['STATUS']
         print(info)
+            
         return 'success', 200
     else:
         abort(400)
