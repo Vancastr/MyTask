562db7bb95536a69dc27e715a7ab5d75c55c484f
Ivan Tukmakov <62853281+Vancastr@users.noreply.github.com>
2023-12-28 21:36:19 +0300

    regexp

--- server.py
+++ server.py
@@ -101,7 +101,7 @@ def webhook_out():
             print(res)
             full = requests.get(address, headers={"Authorization": fin})
             print(full.text)
-            reg = r'{"id".*"ID":' + f'"{ID}"'
+            reg = r'{[\w\d" А-Яа-я,:\-\.]*{[\w\d" А-Яа-я,:\-\.]*"ID":' + f'"{ID}"'
             work = re.findall(reg, full.text)
             almost = re.findall(r'"id":".*?",', work[0])
             task_id = almost[0][6:-2]
